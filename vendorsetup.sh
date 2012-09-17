5166fc35742e2f2fa0ca671f7db1d46f61d0d037
superatmel <superatmel@gmail.com>
2012-09-17 13:09:52 +0200

    control for prorpietary files
    Change-Id: I3115f36c954c82ebb515e6f23d9e233d0aa2a4b8

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,10 @@
 # Get proprietary files
 PATH=$PATH:$PWD/vendor/osr/tools ; export PATH
 VENDOR=$PWD/vendor/osr
+if [ ! -d  "$VENDOR/proprietary" ]
+then
+   vendor/osr/./get-prebuilts
+else
+   echo " Propetarios descargados anteriromente"
+fi
 
