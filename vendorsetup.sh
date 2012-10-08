23c61b69985bec34698dc72f1c5007fbc75f3c3e
shawe <shawe.ewahs@gmail.com>
2012-10-08 14:43:07 +0200

    Innitial support for HackBerry

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,6 +15,7 @@ add_lunch_combo osr_grouper-userdebug
 add_lunch_combo osr_i9100-userdebug
 add_lunch_combo osr_i9300-userdebug
 add_lunch_combo osr_mk802-userdebug
+add_lunch_combo osr_hackberry-userdebug
 add_lunch_combo osr_n7000-userdebug
 add_lunch_combo osr_tf201-userdebug
 
