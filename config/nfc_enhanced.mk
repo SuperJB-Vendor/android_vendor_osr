92bc8dccde998b795847b159f4097d06bcd9080c
podxboq <podxboq@gmail.com>
2013-03-11 22:58:18 +0100

    NFC config file don't need

--- config/nfc_enhanced.mk
+++
@@ -1,3 +0,0 @@
-# This device supports OSR enhanced NFC
-PRODUCT_COPY_FILES += \
-    vendor/osr/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
